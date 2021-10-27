bb67d386319484e3631dd9da7ee0bc076f6dd565
Vladimir Solovyov <leilyngale@icloud.com>
2021-10-27 23:55:24 +0800

    initialize the project

---
+++ intro_CV/intro_CV/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  intro_CV
+//
+//  Created by mb pro on 27.10.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
